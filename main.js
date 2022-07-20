c2a4a1996cf91d29f1dcecefbb02402ca668e127
Howgs <howgs111@gmail.com>
2022-07-20 15:18:15 -0300

    Trocando repositório

---
+++ main.js
@@ -0,0 +1,9 @@
+function tocaPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaClap () { 
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaPom;
