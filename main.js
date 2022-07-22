6ffde790ddf87a7179e1a039883b524eb9837d7c
Howgs <howgs111@gmail.com>
2022-07-22 15:20:51 -0300

    listas

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ function tocaPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaPom;
-
 function tocaClap () { 
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.tecla_clap').onclick = tocaClap
 
-document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaPom;
