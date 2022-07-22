8692eb973baf19962ddeedea2b0cfdfcb7f81042
Howgs <howgs111@gmail.com>
2022-07-22 14:50:19 -0300

    add a função clap

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ function tocaPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
+document.querySelector('.tecla_pom').onclick = tocaPom;
+
 function tocaClap () { 
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaPom;
+document.querySelector('.tecla_clap').onclick = tocaClap
+
+document.querySelectorAll('.tecla');
