2efae7f1b4832d51a7530c8d9a99c5654baec951
Howgs <howgs111@gmail.com>
2022-07-28 15:34:32 -0300

    textos dinâmicos

--- main.js
+++ main.js
@@ -7,15 +7,22 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0
 
 
-
 while (contador < listaDeTeclas.length) {
-    
-    listaDeTeclas[contador].onclick = function () { tocaSom('#som_tecla_pom')
 
+    const instrumento = listaDeTeclas[contador].classList[1]
+
+    const tecla = listaDeTeclas[contador]
+
+//template string
+    const idAudio = `#som_${instrumento}` 
+
+    tecla.onclick = function () { 
+        tocaSom(idAudio)
     }
 
     contador = contador + 1
 
-    console.log(contador)
-
+    //console.log(contador)
+    //console.log(instrumento)
+   //console.log(idAudio)
 }
