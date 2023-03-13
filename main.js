010b709f3d7986ab2e70e8d53d3f1dde4b065c80
Howgs <howgs111@gmail.com>
2023-03-13 14:59:46 -0300

    Trocando while por for

--- main.js
+++ main.js
@@ -4,9 +4,8 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
 const tecla = listaDeTeclas[contador];
 
@@ -25,6 +24,7 @@ const idAudio = `#som_${instrumento}`;
     contador = contador + 1;
 
     console.log(contador);
+
 }
 
 
